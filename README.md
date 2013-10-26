ea73db5d8fa9541c801af4c75e1551b44e70a4e3
Jennifer Nelson <jenniferwnelson@sbcglobal.net>
2013-10-26 13:12:51 -0700

    First commit and readme update.

---
+++ README.md
@@ -0,0 +1,13 @@
+# [digginit](http://digginit.herokuapp.com)
+
+Allows users to create public and private Markdown-based wikis.
+
+Setup
+-----
+
+
+
+
+Usage
+-----
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Digginit::Application.config.secret_key_base = 'e17d96256d9e8ef8cadb503331787793bf52e465f9cbcb611b699471ce2b5d2f28b55795413acc23d46e52fe60423c786daf1b93e536d8586872a89ec4844d46'
