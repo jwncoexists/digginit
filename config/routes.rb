20d328afb1350fa9b0904a18d5e15cc3231a665f
Jennifer Nelson <jenniferwnelson@sbcglobal.net>
2013-10-29 07:08:49 -0700

    Added User Model with password protection

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Digginit::Application.routes.draw do
+  resources :users
+
   root to: 'welcome#index'
   match "about" => 'welcome#about', via: :get 
+  resources :users, only: [:create, :destroy, :show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
